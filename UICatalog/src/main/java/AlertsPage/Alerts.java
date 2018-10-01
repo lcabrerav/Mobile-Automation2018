dd2e9e024c580f5640562e3785476f917160a258
lcabrerav <luisjoelcabrera@gmail.com>
2018-10-01 19:13:50 -0400

    test

--- UICatalog/src/main/java/AlertsPage/Alerts.java
+++ UICatalog/src/main/java/AlertsPage/Alerts.java
@@ -2,9 +2,6 @@ package AlertsPage;
 
 import common.Base;
 
-/**
- * Created by mrahman on 1/15/17.
- */
 public class Alerts extends Base{
     public void showActionSheetSimple(){
         clickByXpath("//UIAApplication[1]/UIAWindow[2]/UIATableView[1]/UIATableCell[1]");
