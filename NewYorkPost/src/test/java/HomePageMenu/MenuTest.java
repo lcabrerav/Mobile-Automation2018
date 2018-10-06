8e82359e53a383afaabc0a36433041998d9e1ab1
lcabrerav <luisjoelcabrera@gmail.com>
2018-10-05 21:29:27 -0400

    final commit

--- NewYorkPost/src/test/java/HomePageMenu/MenuTest.java
+++ NewYorkPost/src/test/java/HomePageMenu/MenuTest.java
@@ -13,23 +13,23 @@ public class MenuTest extends Base {
     public void init (){
         homePageMenu = PageFactory.initElements(ad,HomePageMenu.class);
     }
-    @Test(priority = 2, enabled = false)
+    @Test
     public void menuSearchBoxTest() throws InterruptedException {
         homePageMenu.setMenuSearchBox();
     }
-    @Test(priority = 1, enabled = false)
+    @Test
     public void topNewsTest(){
         homePageMenu.setTopNews();
     }
-    @Test(priority = 0, enabled = true)
+    @Test
     public void menuNewsTest() throws InterruptedException {
         homePageMenu.setMenuNews();
     }
-    @Test(priority = 3, enabled = false)
+    @Test
     public void notificationTest(){
         homePageMenu.setNotifications();
     }
-    @Test(priority = 4, enabled = false)
+    @Test
     public void signUpTest() throws InterruptedException {
         homePageMenu.setSignUp();
     }
